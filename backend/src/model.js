a368943580bce93d7c1662d6cdb427699911e161
LLCoolDave <llcooldave@gmail.com>
2018-12-20 21:58:38 +0100

    Fix up datatypes in the database models.

--- backend/src/model.js
+++ backend/src/model.js
@@ -4,7 +4,7 @@ const StartPoint = new mongoose.Schema({
   type: String, // One of 'absolute', 'startOf', 'endOf'
   setup: Number, // Length of setup time in ms
   time: Number, // Used for type === 'absolute'; timestamp in ms since epoch
-  ref: ObjectId, // Used for type === 'startOf', 'endOf'; reference to ScheduleElement this is relative to
+  ref: mongoose.Schema.Types.ObjectId, // Used for type === 'startOf', 'endOf'; reference to Element this is relative to
   offset: Number, // Offset from startpoint in ms (not very sensible for type === 'absolute')
   actualTime: Number // Timestamp in ms since epoch, actual time this item started (takes precedence)
 })
@@ -13,7 +13,7 @@ const EndPoint = new mongoose.Schema({
   type: String, // One of 'absolute', 'startOf', 'endOf', 'duration'
   setup: Number, // Length of setup time in ms
   time: Number, // Used for type === 'absolute'; timestamp in ms since epoch
-  ref: ObjectId, // Used for type === 'startOf', 'endOf'; reference to ScheduleElement this is relative to
+  ref: mongoose.Schema.Types.ObjectId, // Used for type === 'startOf', 'endOf'; reference to Element this is relative to
   duration: Number, // Used for type === 'duration'; length of Element in ms
   offset: Number, // Offset from endpoint in ms (not very sensible for type === 'absolute', not used for 'duration')
   actualTime: Number // Timestamp in ms since epoch, actual time this item ended (takes precedence)
@@ -24,8 +24,8 @@ const People = new mongoose.Schema({
   name: String
 })
 
-const ScheduleElement = new mongoose.Schema({
-  _id: ObjectId,
+const Element = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
   name: String,
   data: Object, // Typed by parent.dataSchema, contains additional data fields that are schedule specific
   foreignDataModel: String, // Type of data referenced from external database (i.e. user tool)
@@ -39,11 +39,11 @@ const ScheduleElement = new mongoose.Schema({
 })
 
 const Schedule = new mongoose.Schema({
-  _id: ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  dataSchema: Object, // Describes the schema applied to the 'data' field of ScheduleElements belonging to this schedule
+  dataSchema: Object, // Describes the schema applied to the 'data' field of Elements belonging to this schedule
   offset: Number, // Time offset in ms to Event start where this schedule begins.  ToDo do we want this? Useful for validation for constraints?
-  elements: [{ type: ObjectId, ref: 'scheduleelements' }],
+  elements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'elements' }],
   constraints: {
     minimumRequiredPeople: Number,
     overlap: Boolean,
@@ -58,12 +58,12 @@ const Schedule = new mongoose.Schema({
 })
 
 const Event = new mongoose.Schema({
-  _id: ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   usertoolRef: String, // reference to event from users tool
   name: String, // Human readable name for the Event to be displayed
   startTime: Number, // Timestamp in ms since epoch # ToDo will be fetched from users tool in future
   endTime: Number, // Timestamp in ms since epoch # ToDo will be fetched from users tool in future
-  schedules: [{ type: ObjectId, ref: 'schedules' }],
+  schedules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'schedules' }],
   constraintSettings: {
     peopleAvailable: Boolean,
     preferredTimeslots: Boolean
@@ -75,8 +75,8 @@ const UpdateAction = new mongoose.Schema({
   id: String, // Reference to be modified
   action: String, // "set", "insert", "delete", "addItem", "removeItem"
   path: String, // Nested Attribute to modify
-  oldValue: Mixed, // Original value of <id>.path
-  newValue: Mixed // Value to set <id>.path to
+  oldValue: mongoose.Schema.Types.Mixed, // Original value of <id>.path
+  newValue: mongoose.Schema.Types.Mixed // Value to set <id>.path to
 })
 
 const Transformation = new mongoose.Schema({
@@ -88,6 +88,6 @@ const Transformation = new mongoose.Schema({
 
 export default {
   models: {
-    StartPoint, EndPoint, People, ScheduleElement, Schedule, Event, UpdateAction, Transformation
+    StartPoint, EndPoint, People, Element, Schedule, Event, UpdateAction, Transformation
   }
 }

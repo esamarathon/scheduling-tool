88d117227837cdec9b19a551aee1878b64eac4a3
mangohatface <mangohatface@bigfatsodacompany.com>
2019-01-16 18:35:58 +0000

    Hookup mongo to models

--- backend/src/model.js
+++ backend/src/model.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import './db';
 
 const StartPoint = new mongoose.Schema({
   type: String, // One of 'absolute', 'startOf', 'endOf'
@@ -6,8 +7,8 @@ const StartPoint = new mongoose.Schema({
   time: Number, // Used for type === 'absolute'; timestamp in ms since epoch
   ref: mongoose.Schema.Types.ObjectId, // Used for type === 'startOf', 'endOf'; reference to Element this is relative to
   offset: Number, // Offset from startpoint in ms (not very sensible for type === 'absolute')
-  actualTime: Number // Timestamp in ms since epoch, actual time this item started (takes precedence)
-})
+  actualTime: Number, // Timestamp in ms since epoch, actual time this item started (takes precedence)
+});
 
 const EndPoint = new mongoose.Schema({
   type: String, // One of 'absolute', 'startOf', 'endOf', 'duration'
@@ -16,13 +17,13 @@ const EndPoint = new mongoose.Schema({
   ref: mongoose.Schema.Types.ObjectId, // Used for type === 'startOf', 'endOf'; reference to Element this is relative to
   duration: Number, // Used for type === 'duration'; length of Element in ms
   offset: Number, // Offset from endpoint in ms (not very sensible for type === 'absolute', not used for 'duration')
-  actualTime: Number // Timestamp in ms since epoch, actual time this item ended (takes precedence)
-})
+  actualTime: Number, // Timestamp in ms since epoch, actual time this item ended (takes precedence)
+});
 
 const People = new mongoose.Schema({
   // ToDo, this is still very open
-  name: String
-})
+  name: String,
+});
 
 const Element = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
@@ -31,12 +32,12 @@ const Element = new mongoose.Schema({
   foreignDataModel: String, // Type of data referenced from external database (i.e. user tool)
   foreignData: String, // Referenced data ID from external database
   constraints: {
-    preferredTimeslot: String
+    preferredTimeslot: String,
   },
   people: [People],
   start: StartPoint,
-  end: EndPoint
-})
+  end: EndPoint,
+});
 
 const Schedule = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
@@ -47,15 +48,15 @@ const Schedule = new mongoose.Schema({
   constraints: {
     minimumRequiredPeople: Number,
     overlap: Boolean,
-    minimumSetupTime: Number
+    minimumSetupTime: Number,
   },
   defaults: {
     name: String,
     setup: Number,
     teardown: Number,
-    duration: Number
-  }
-})
+    duration: Number,
+  },
+});
 
 const Event = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
@@ -66,9 +67,9 @@ const Event = new mongoose.Schema({
   schedules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'schedules' }],
   constraintSettings: {
     peopleAvailable: Boolean,
-    preferredTimeslots: Boolean
-  }
-})
+    preferredTimeslots: Boolean,
+  },
+});
 
 const UpdateAction = new mongoose.Schema({
   idType: String, // Type of Element the ID references
@@ -76,18 +77,27 @@ const UpdateAction = new mongoose.Schema({
   action: String, // "set", "insert", "delete", "addItem", "removeItem"
   path: String, // Nested Attribute to modify
   oldValue: mongoose.Schema.Types.Mixed, // Original value of <id>.path
-  newValue: mongoose.Schema.Types.Mixed // Value to set <id>.path to
-})
+  newValue: mongoose.Schema.Types.Mixed, // Value to set <id>.path to
+});
 
 const Transformation = new mongoose.Schema({
   time: Date, // time when this transaction was performed
   userID: String, // user ID from the user tool
   type: String, // Kind of transformation
-  actions: [UpdateAction]
-})
+  actions: [UpdateAction],
+});
 
-export default {
-  models: {
-    StartPoint, EndPoint, People, Element, Schedule, Event, UpdateAction, Transformation
-  }
-}
+export const schemas = {
+  StartPoint, EndPoint, People, Element, Schedule, Event, UpdateAction, Transformation,
+};
+
+export const models = {
+  StartPoint: mongoose.model('startpoint', StartPoint),
+  EndPoint: mongoose.model('endpoint', EndPoint),
+  People: mongoose.model('people', People),
+  Element: mongoose.model('element', Element),
+  Schedule: mongoose.model('schedule', Schedule),
+  Event: mongoose.model('event', Event),
+  UpdateAction: mongoose.model('updateaction', UpdateAction),
+  Transformation: mongoose.model('transformation', Transformation),
+};
